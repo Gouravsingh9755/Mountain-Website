7cf098db4b613f4e49e7c5e33eecb6684a886b0b
Gouravsingh9755 <gouravsingh97555@gmail.com>
2024-02-02 18:35:48 +0530

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,12 +4,20 @@ import gsap from "gsap";
 
 const Loading = () => {
     const app = useRef<HTMLDivElement>(null);
+    const aref =useRef(null)
+    
     useEffect(() => {
+      const lCont  = aref.current;
+
+      console.log(lCont);
+
+
     
         let ctx = gsap.context(() => {
           
 
-            gsap.to(".loading", { display:"none",pointerEvents:"all",delay :0, duration: .4 });
+            gsap.to(".loading", { display:"none",delay :2, duration: .4 });
+
         
             
         
@@ -21,7 +29,7 @@ const Loading = () => {
 
     return (
 
-    <div data-scroll-section data-scroll  className="loading w-screen h-[1000vh] bg-red-300 fixed top-0 left-0 right-0 bottom-0 z-[99]">
+    <div ref={aref} className="loading w-screen h-[1000vh] bg-black fixed top-0 left-0 right-0 bottom-0 z-[99] flex justify-center pointer-events-auto overflow-hidden"> <h1 className="h1 text-[70px] font-bold text-white mt-[20%]">MNTN</h1>
     </div>
     
   )
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -21,11 +21,20 @@ const Top = () => {
                 </h3>
               </nav>
 
-              <div id="hero_boxheading"
-              data-scroll
-              data-scroll-speed={-9.5}
+            
+
+
+          <div className="imgbox h-[100vh] relative w-full">
+            <div
+              style={mystyle}
+              className="overlay z-20   relative top-0 left-0 right-0 bottom-0 h-[150%] w-[100%] opacity-[0.6] "
+            >
+
+         <div id="hero_boxheading"
+             data-scroll
+             data-scroll-speed={-9.5}
              
-             className="hero-content bg-red-800 h-[52vh] w-[100vw] absolute xl:top-[0%] 2xl:top-[-30%] left-0 z-[20]">
+             className="hero-content  h-[52vh] w-[100vw] absolute top-[0%] left-0 z-[20]">
 
               <h3 id="up_heading"
               className="font-black text-xl font-sans tracking-[3px] absolute z-30 top-[20%]  left-60   text-yellow-300 selection:text-yellow-500">
@@ -43,13 +52,6 @@ const Top = () => {
             <img className="downarrow  absolute z-30 top-[92%]   left-[19%] h-[5vh] " src="https://media0.giphy.com/media/a2e4I5koserzi0buol/giphy.webp" alt="" />
 
             </div>
-
-
-          <div className="imgbox h-[100vh] relative w-full">
-            <div
-              style={mystyle}
-              className="overlay z-20  bg-green-500 relative top-0 left-0 right-0 bottom-0 h-[150%] w-[100%] opacity-[0.6] "
-            >
 
           
 
